091e2ba7d2f9d41db983f1e810d49cc11133c6c0
Diamondz213 <98407444+Diamondz213@users.noreply.github.com>
2022-05-05 12:29:32 -0400

    FinalBasicGame
    Revamp of Basic game for final project

--- BasicGame/Assets/Scripts/Timer.cs
+++ BasicGame/Assets/Scripts/Timer.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
+    public TextMeshProUGUI TimerText;
+    public CollisionDetector collisionScript;
+
     public float countdownTimer = 10f;
-    public float countingDown = 60f;
+    public float countingDown = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,16 +23,21 @@ public class Timer : MonoBehaviour
     void Update()
     {
         countingDown -= 1 * Time.deltaTime;
+        TimerText.text = "Time Left: " + countdownTimer;
+
+        if (collisionScript.hitCounter >= 1)
+        {
+            countingDown = 999;
+        }
 
         if (countingDown <= 0)
         {
             countdownTimer -= 1;
-            countingDown = 60f;
-            print("The countdown is " + countdownTimer);
+            countingDown = 1f;
 
             if(countdownTimer <= 0)
             {
-                countdownTimer = 10;
+                SceneManager.LoadScene(2);
             }
         }
     }
